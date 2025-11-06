f459e007ee2b7c39d5e7675185b6810fa510a267
piyushkhatri968 <piyushkhatri968@gmail.com>
2025-11-06 13:47:28 +0500

    fix vertual balance issue

--- routes/wallet.js
+++ routes/wallet.js
@@ -65,9 +65,11 @@ router.post("/withdraw", async (req, res) => {
     await userBalance.save();
 
    // For virtual (notification) balance: when a user withdraws, add back to currentBalance
-   const currentBalance = await NotificationBalance.findOne({userId})
-  currentBalance.currentBalance  = currentBalance.currentBalance + amount 
-  await NotificationBalance.save()
+   await NotificationBalance.findOneAndUpdate(
+     { userId },
+     { $inc: { currentBalance: amount } },
+     { new: true, upsert: true }
+   );
 
     const user = await User.findById(userId);
 
