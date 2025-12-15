9da458801db37a3624df09f2263d39b71def07ef
piyushkhatri968 <piyushkhatri968@gmail.com>
2025-12-15 19:42:56 +0500

    .

--- app.js
+++ app.js
@@ -186,7 +186,7 @@ mongoose
     console.error("Error connecting to MongoDB:", error);
   });
 
-const PORT = process.env.PORT || 5008;
+const PORT = process.env.PORT || 5002;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
